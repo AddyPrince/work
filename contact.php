2c485b0921ca4754b4e816ba5dcb4679819f6da2
saitham <saitham.vo@gmail.com>
2020-07-19 14:25:09 +0700

    first commit

--- contact.php
+++ contact.php
@@ -1,6 +1,43 @@
 <?php 
 include 'head.php'
 ?>
+
+<!-- Navber -->
+<nav id="navbar" class="navbar navbar-expand-lg fixed-top navbar-dark bg-alpha">
+    <div class="container">
+      <a class="navbar-brand" href="#">
+        <img src="assets/image/logo1.jpg" width="45" height="45" alt="" style="border-radius: 5px;">
+      </a>
+
+      <span class="navbar-text text-white collapse navbar-collapse">
+        ICON-ENGINEERING SOLUTIONS
+      </span>
+
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarKey"
+        aria-controls="#navbarKey" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarKey">
+        <ul class="navbar-nav ml-auto text-center">
+          <li class="nav-item">
+          <a class="nav-link" href="index.php">Home<span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+          <a class="nav-link" href="about.php">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="blog.php">Blog</a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="contact.php">Contact</a>
+          </li>
+        </ul>
+
+      </div>
+    </div>
+  </nav>
+
 <!-- TOP -->
 <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
     <div class="carousel-inner">
--- index.php
+++ index.php
@@ -1,6 +1,42 @@
 <?php 
 include 'head.php' ;
 ?>
+<!-- Navber -->
+<nav id="navbar" class="navbar navbar-expand-lg fixed-top navbar-dark bg-alpha">
+    <div class="container">
+      <a class="navbar-brand" href="#">
+        <img src="assets/image/logo1.jpg" width="45" height="45" alt="" style="border-radius: 5px;">
+      </a>
+
+      <span class="navbar-text text-white collapse navbar-collapse">
+        ICON-ENGINEERING SOLUTIONS
+      </span>
+
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarKey"
+        aria-controls="#navbarKey" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarKey">
+        <ul class="navbar-nav ml-auto text-center">
+          <li class="nav-item">
+          <a class="nav-link active" href="index.php">Home<span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+          <a class="nav-link" href="about.php">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="blog.php">Blog</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="contact.php">Contact</a>
+          </li>
+        </ul>
+
+      </div>
+    </div>
+  </nav>
+  
   <!-- image top -->
   <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
     <ol class="carousel-indicators">
