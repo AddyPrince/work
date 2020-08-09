71688eea1b87d6cc0f9f0c02ed329b47d773284e
Surasak sincharoen <moomdate@gmail.com>
2020-08-09 17:29:11 +0700

    fix file

--- controller/langController.php
+++ controller/langController.php
@@ -12,19 +12,5 @@ if(isset($_GET['lang'])){
     $langParam = 'th';
 }
 
-$langCon= array(
-    "th" => array(
-        "home"=>"หน้าแรก",
-        "about"=>"เกี่ยวกับ",
-        "service"=>"บริการของเรา",
-        "portfolio"=>"ผลงาน",
-        "contact"=>"ติดต่อ",
-    ),
-    "en" =>  array(
-        "home"=>"Home",
-        "about"=>"About",
-        "service"=>"Services",
-        "portfolio"=>"Portfolio",
-        "contact"=>"Contact",
-    ));
+include 'language.config.inc.php';
 ?>
---
+++ controller/language.config.inc.php
@@ -0,0 +1,17 @@
+<?php
+$langCon= array(
+    "th" => array(
+        "home"=>"หน้าแรก",
+        "about"=>"เกี่ยวกับ",
+        "service"=>"บริการของเรา",
+        "portfolio"=>"ผลงาน",
+        "contact"=>"ติดต่อ",
+    ),
+    "en" =>  array(
+        "home"=>"Home",
+        "about"=>"About",
+        "service"=>"Services",
+        "portfolio"=>"Portfolio",
+        "contact"=>"Contact",
+    ));
+?>
